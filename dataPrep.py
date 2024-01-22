3466380ff0c93104a0fc9b854694d8d4b64fa9fa
Samin Sarker <saminsarker05@gmail.com>
2024-01-21 22:48:35 -0500

    data normalization

--- dataPrep.py
+++ dataPrep.py
@@ -1,4 +1,5 @@
 '''
+
 '''
 
 import os
@@ -12,4 +13,15 @@ x_train = pd.read_hdf('path-to-x-train-data')
 y_train = pd.read_hdf('path-to-y-train-data')
 x_test = pd.read_hdf('path-to-x-test-data')
 y_test = pd.read_hdf('path-to-y-test-data')
+
+# normalize data by substracting mean and dividing by std
+
+variables = pd.read_csv('meta-data-path', index_col=0)
+norm = variables[variables['type'].isin(['Interventions', 'Labs', 'Vitals'])]
+
+for index, data in norm.iterrows():
+  x_train[index] -= data['mean']
+  x_valid[index] -= data['mean']
+  x_train[index] /= (data['std'] + 1e-12)
+  x_valid[index] /= (data['std'] + 1e-12)
 

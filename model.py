fdf080b5ca579b90d07c1ad58eef9fc494239a9d
Samin Sarker <saminsarker05@gmail.com>
2024-01-21 23:07:44 -0500

    model finished

--- model.py
+++ model.py
@@ -19,9 +19,19 @@ data = Input((None, x_train.shape[-1]), name='data')
 mask = Masking(0, name='input_masked')(data)
 
 
-# stack the LSTM layers
+# stack the LSTM layers each with 128 units
 
 lstm_kwargs = {'dropout': 0.25, 'recurrent_dropout': 0.1, 'return_sequences': True, 'implementation': 2}
 lstm = LSTM(128, name='lstm', **lstm_kwargs)(mask)
 
+
+# define output layer with sigmoid activation function for classification
+
+output = TimeDistributed(Dense(1, activation='sigmoid'), name='output')(lstm)
+
+
+# use RMS optimizer and binary cross entropy as loss function
+
+optimizer = RMSprop(lr=0.005)
+model.compile(optimizer=optimizer, loss='binary_crossentropy')
 

f242677267ccc5d94c38d66cc86aba2428cf052e
Samin Sarker <saminsarker05@gmail.com>
2024-01-21 23:05:21 -0500

    adding lstm layers

--- model.py
+++ model.py
@@ -13,3 +13,15 @@ from tensorflow.keras.layers import LSTM, Dense, TimeDistributed, Masking
 from tensorflow.keras.optimizers import RMSprop
 
 
+# define input and mask out padded values
+
+data = Input((None, x_train.shape[-1]), name='data')
+mask = Masking(0, name='input_masked')(data)
+
+
+# stack the LSTM layers
+
+lstm_kwargs = {'dropout': 0.25, 'recurrent_dropout': 0.1, 'return_sequences': True, 'implementation': 2}
+lstm = LSTM(128, name='lstm', **lstm_kwargs)(mask)
+
+

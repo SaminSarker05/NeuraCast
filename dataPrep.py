171fa44e965b534e115aff8eba26dda7bf337e3d
Samin Sarker <saminsarker05@gmail.com>
2024-01-21 22:52:38 -0500

    missing data points corrected

--- dataPrep.py
+++ dataPrep.py
@@ -11,8 +11,8 @@ import tensorflow.keras as keras
 
 x_train = pd.read_hdf('path-to-x-train-data')
 y_train = pd.read_hdf('path-to-y-train-data')
-x_test = pd.read_hdf('path-to-x-test-data')
-y_test = pd.read_hdf('path-to-y-test-data')
+x_valid = pd.read_hdf('path-to-x-valid-data')
+y_valid = pd.read_hdf('path-to-y-valid-data')
 
 # normalize data by substracting mean and dividing by std
 
@@ -24,4 +24,15 @@ for index, data in norm.iterrows():
   x_valid[index] -= data['mean']
   x_train[index] /= (data['std'] + 1e-12)
   x_valid[index] /= (data['std'] + 1e-12)
+
+
+# fill in missing data points with forward fill and fillna
+
+fill = variables[variables['type'].isin(['Vitals', 'Labs'])].index
+
+x_train[fill] = x_train.groupby(level=0)[fillvars].ffill()
+x_valid[fill] = x_valid.groupby(level=0)[fillvars].ffill()
+
+x_train.fillna(value=0, inplace=True)
+x_valid.fillna(value=0, inplace=True)
 
